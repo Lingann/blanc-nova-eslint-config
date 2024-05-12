ec4edb4acb7d2c8356df2ce31e85b1ab8659db95
wanglingan <wanglingan@mysteel.com>
2024-05-12 21:10:32 +0800

    ✨ feat: 修复eslint-config-vue

--- packages/vue/index.js
+++ packages/vue/index.js
@@ -1,10 +1,10 @@
 'use strict'
 module.exports = {
+  extends: ["plugin:vue/vue3-recommended'"],
   overrides: [
     {
       files: ['*.vue'],
       parser: 'vue-eslint-parser',
-      extends: ["plugin:vue/vue3-recommended'"],
     },
   ],
   rules: {
